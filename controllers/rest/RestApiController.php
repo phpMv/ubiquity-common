e6076a6d67d1aa9cc2f11800a92a065aa79f8ab6
jcheron <myaddressmail@gmail.com>
2020-02-17 03:06:30 +0100

    update rest-api to ubiquity 2.3

--- controllers/rest/RestApiController.php
+++ controllers/rest/RestApiController.php
@@ -1,7 +1,8 @@
 <?php
 namespace PhpBenchmarksUbiquity\RestApi\controllers\rest;
 
-use Ubiquity\controllers\rest\RestController;
+use Ubiquity\controllers\rest\RestBaseController;
+use Ubiquity\controllers\Startup;
 use Ubiquity\events\EventsManager;
 use Ubiquity\contents\normalizers\NormalizersManager;
 use PhpBenchmarksUbiquity\RestApi\normalizer\UserNormalizer;
@@ -14,22 +15,36 @@ use PhpBenchmarksRestData\Comment;
 use PhpBenchmarksRestData\Service;
 
 /**
+ *
  * @route("/benchmark/rest","inherited"=>false,"automated"=>false)
  * @rest("resource"=>"")
  */
-class RestApiController extends RestController{
-	public function initialize(){
-		
+class RestApiController extends RestBaseController {
+
+	public function __construct() {
+		if (! \headers_sent()) {
+			$this->config = Startup::getConfig();
+			$this->server = $this->_getRestServer();
+			$this->responseFormatter = $this->_getResponseFormatter();
+			$this->server->_setContentType($this->contentType);
+		}
 	}
-	
+
+	public function initialize() {}
+
 	/**
 	 * Returns all objects for the resource $model
+	 *
 	 * @route("cache"=>false)
 	 */
 	public function index() {
 		EventsManager::trigger(DefineLocaleEventListener::EVENT_NAME);
-		NormalizersManager::registerClasses([User::class=>UserNormalizer::class,CommentType::class=>CommentTypeNormalizer::class,Comment::class=>CommentNormalizer::class]);
-		$datas=NormalizersManager::normalizeArray_(Service::getUsers());
+		NormalizersManager::registerClasses([
+			User::class => UserNormalizer::class,
+			CommentType::class => CommentTypeNormalizer::class,
+			Comment::class => CommentNormalizer::class
+		]);
+		$datas = NormalizersManager::normalizeArray_(Service::getUsers());
 		echo $this->_getResponseFormatter()->toJson($datas);
 	}
 }
